03bbadbcc2e704aebde3635d3dd950c8b028cd3e
Ian Knox <knoxid@gmail.com>
2014-07-19 19:43:01 -0600

    new migration

---
+++ db/migrate/1405820330_users_venues.rb
@@ -0,0 +1,24 @@
+class UsersVenues < ActiveRecord::Migration
+  def up
+    create_table :venues do |t|
+      t.string :title
+      t.string :position
+      t.string :icon
+      t.string :marker_name
+    end
+
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :password
+      t.string :birthday
+      t.date :join_date
+    end
+
+    def down
+      drop_table :users
+      drop_table :venues
+    end
+  end
+end
