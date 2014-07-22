3a7c0c0fbbc7c001a8c350c2f5ebdd94c5ae78bf
Ian Knox <knoxid@gmail.com>
2014-07-22 11:02:00 -0600

    fixed admin access and reorganized admin endpoints

--- app.rb
+++ app.rb
@@ -16,19 +16,22 @@ class App < Sinatra::Application
 
   get "/" do
     venues = filter_list(sort_list(@db.get_venues, params[:sort_venues]), params[:filter])
-    users = sort_list(@db.get_users(session[:id]), params[:sort_users])
     user = @db.get_user(session[:id])
-    if session[:id] == 1
+    if is_admin?
       erb :admin, :locals => {
-        :users => users,
         :venues => venues,
-        :list => params[:list]
       }
     else
       erb :home, :locals => {
         :cur_user => user,
         :venues => venues
       }
+    end
+  end
+
+  before "/admin/*" do
+    unless is_admin?
+      redirect "/"
     end
   end
 
@@ -41,42 +44,8 @@ class App < Sinatra::Application
     erb :register
   end
 
-  get "/users/:id/edit" do
-    erb :user_edit, :locals => {:user => @db.get_user(params[:id])}
-  end
-
   get "/users/:id/pw/edit" do
-    erb :pw, :locals => {:user => @db.get_user(params[:id])}
-  end
-
-  get "/venues/new" do
-    erb :add_venue
-  end
-
-  get "/venues/:id" do
-    erb :venue, :locals => {
-      :venue => @db.get_venue(params[:id]),
-      :cur_user => @db.get_user(session[:id])
-    }
-  end
-
-  get "/venues" do
-    erb :venues, :locals => {
-      :venues => sort_list(@db.get_venues, "title"),
-      :cur_user => @db.get_user(session[:id])
-    }
-  end
-
-  get "/venues/:id/edit" do
-    erb :venue_edit, :locals => {:venue => @db.get_venue(params[:id])}
-  end
-
-  post "/" do
-    check_login(params[:email], params[:password])
-  end
-
-  post "/users" do
-    check_reg(params)
+    erb :pw, :locals => {:cur_user => @db.get_user(params[:id])}
   end
 
   post "/users/:id/pw" do
@@ -93,6 +62,52 @@ class App < Sinatra::Application
     end
   end
 
+  get "/admin/venues/new" do
+    erb :add_venue
+  end
+
+  get "/admin/venues" do
+    venues = filter_list(sort_list(@db.get_venues, params[:sort]), params[:filter])
+
+    erb :ad_venues, :locals => {:venues => venues}
+  end
+
+  get "/admin/users" do
+    users = sort_list(@db.get_users(session[:id]), params[:sort])
+    erb :ad_users, :locals => {:users => users}
+  end
+
+  get "/venues" do
+    erb :venues, :locals => {
+      :venues => sort_list(@db.get_venues, "title"),
+      :cur_user => @db.get_user(session[:id])
+    }
+  end
+
+  get "/venues/:id" do
+    erb :venue, :locals => {
+      :venue => @db.get_venue(params[:id]),
+      :cur_user => @db.get_user(session[:id])
+    }
+  end
+
+  get "/admin/users/:id/edit" do
+    erb :user_edit, :locals => {:user => @db.get_user(params[:id])}
+  end
+
+  get "/admin/venues/:id/edit" do
+    erb :venue_edit, :locals => {:venue => @db.get_venue(params[:id])}
+  end
+
+  post "/" do
+    check_login(params[:email], params[:password])
+  end
+
+  post "/users" do
+    check_reg(params)
+  end
+
+
   post "/venues" do
     desc = params[:description].length
     if desc > 255
@@ -107,27 +122,31 @@ class App < Sinatra::Application
 
   patch "/venues/:id" do
     @db.update_venue(params)
-    redirect "/venues/#{params[:id]}"
+    redirect back
   end
 
   patch "/users/:id" do
     @db.update_user(params)
-    redirect "/"
+    redirect back
   end
 
   delete "/users/:id" do
-    flash[:notice] = "#{@db.get_name(params[:id])} deleted"
+    flash[:notice] = "#{@db.get_user(params[:id])["first_name"]} deleted"
     @db.delete_user(params[:id])
-    redirect "/"
+    redirect back
   end
 
   delete "/venues/:id" do
     flash[:notice] = "#{@db.get_venue(params[:id])["title"]} deleted"
     @db.delete_venue(params[:id])
-    redirect "/venues"
+    redirect back
   end
 
   private
+
+  def is_admin?
+    session[:id] == 1
+  end
 
   def check_reg(params)
     if !check_pw(params[:password], params[:pass_conf])
