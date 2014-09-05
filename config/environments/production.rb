a5958696e14ddbb9e620a398615e0388b928ced0
Ian Knox <knoxid@gmail.com>
2014-09-05 14:22:26 -0600

    added to prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,10 +79,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  config.assets.raise_runtime_errors = true
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
+  ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
