a9731f24c3e3a6074169f2546471ca5a734dbe8a
Ian Knox <knoxid@gmail.com>
2014-09-06 00:07:35 -0600

    added stubhub mailer and songkick events

--- app/controllers/ticketflyevents_controller.rb
+++
@@ -1,8 +0,0 @@
-class TicketflyeventsController < ApplicationController
-
-  def create
-    TicketflyEvents.all.each { |event| Event.create(event) }
-    flash[:notice] = "Ticketfly events successfully pulled"
-    redirect_to root_path
-  end
-end
