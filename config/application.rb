f5537449b1c202123be07a780bad2e5331144d52
Ian Knox <knoxid@gmail.com>
2014-09-11 11:27:24 -0600

    fonts

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,6 @@ Bundler.require(*Rails.groups)
 
 module SoundLocale
   class Application < Rails::Application
-config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
   end
 end
