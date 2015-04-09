8c757fa6067023bd06acdd92dca120dd434cf923
Ian Knox <knoxid@gmail.com>
2015-04-09 05:17:03 -0600

    nav and formatting

--- app/assets/javascripts/events-index.js
+++ app/assets/javascripts/events-index.js
@@ -11,10 +11,28 @@ $(document).ready(function () {
     return (display === 'none')
   }
 
-//  $('.date-content').hide();
+  $('.date-content').hide();
+  $('#close-all').hide();
+
+  $('.toggle-all').on('click', function () {
+    if (isHidden($('#close-all'))) {
+      $('.date-content').show();
+      $('#close-all').show();
+      $('#expand-all').hide();
+    } else {
+      $('.date-content').hide();
+      $('#close-all').hide();
+      $('#expand-all').show();
+    }
+  });
 
   $('.date-header').on('click', function () {
     var content = $(this).siblings('.date-content')
+    var menuHeight = $('#nav-custom').height();
+
+    $('html,body').animate({
+      scrollTop: $(this).offset().top - menuHeight
+    }, 500);
 
     if (isHidden(content)) {
       content.slideDown();
@@ -22,37 +40,15 @@ $(document).ready(function () {
       content.slideUp();
     }
   })
-
-
 });
 
-
-//$(function () {
-//  $('.cal-date').datepicker({ dateFormat: "yy-mm-dd"});
-//});
-//
-//$('.cal-date').datepicker({
-//  onSelect: function (dateText) {
-//    dateText = dateText.toString().replace('/', '').replace('/', '');
-//    $('html,body').animate({
-//      scrollTop: $('#' + dateText + '').offset().top
-//    }, 500);
-//  }
-//});
-
-// Custom case-insensitive :contains
-//$.expr[':'].containsCaseInsensitive = function (a, i, m) {
-//  return $(a).text().toUpperCase()
-//    .indexOf(m[3].toUpperCase()) >= 0;
-//};
-//
-//$('.no-events-container').hide();
 $('.search').on('keyup', function () {
   var search = $(this).val();
   var events = $('.event');
   var results = $('.event:containsCaseInsensitive(' + search + ')');
 
   events.hide().addClass('hidden');
+  $('.date-content').show();
   results.show().removeClass('hidden');
 
   $('.date').each(function () {
@@ -65,6 +61,9 @@ $('.search').on('keyup', function () {
     }
   });
 
+  if (search === '') {
+    $('.date-content').hide();
+  }
 
   if ($('.date-events-container.hidden').length == $('.date-events-container').length) {
     $('.event-list-container').hide();
